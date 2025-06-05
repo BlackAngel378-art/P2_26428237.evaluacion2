d51efe50ddcf4dd7dd4a2572d1bbc3db833093ad
BlackAngel378-art <angelguerra378@gmail.com>
2025-06-04 21:19:44 -0400

    subiendo ultimas correciones

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,21 @@
-/*import express from 'express';
-import path from 'path';
-import indexRouter from './routes/index';
-
-const app = express();
-const port = 3000;
-
-// Configurar 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Configurar rutas
-app.use('/', indexRouter);
-
-// Iniciar servidor
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});*/
 import express from 'express';
 import path from 'path';
-import indexRouter from './routes/index';
-
+import indexRouter from './routes/index.js';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3000;
 
 // Configuración de EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views/index')); // Asegúrate que apunta a tu carpeta views
+app.set('views', path.join(__dirname,'views')); // Asegúrate que apunta a tu carpeta views
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../views'))); // Para archivos estáticos
+
+app.use(express.static(path.join(__dirname,'public'))); // Para archivos estáticos
 
 // Configurar rutas
 app.use('/', indexRouter);
